96222883ffd52bcf8dbe8f3576776e81272b4255
adi-ray <adityadns03@gmail.com>
2025-05-18 00:57:46 +0530

    feat: implement user registration functionality with bcrypt encryption

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,28 @@
+const UserService = require("../services/user-service");
+
+const userService = new userService();
+
+const create = async (req, res) => {
+  try {
+    const response = await userService.create({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    return res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: response,
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      err: error,
+      data: {},
+    });
+  }
+};
+
+module.exports = { create };
